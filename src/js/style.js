4bcb0079509f3ab392b6426fe326d2cee9a703f2
timkeane <tkeane@doitt.nyc.gov>
2018-10-25 16:28:03 -0400

    implement containsXY for ol.geom.Point

--- src/js/style.js
+++ src/js/style.js
@@ -8,9 +8,24 @@ import Text from 'ol/style/Text'
 import Fill from 'ol/style/Fill'
 import nycOl from 'nyc-lib/nyc/ol' 
 
+import Point from 'ol/geom/Point'
+
+Point.prototype.containsXY = function(x, y) {
+  const coord = this.getCoordinates()
+  return coord[0] === x && coord[1] === y
+}
+
 const style = (feature, resolution) => {
-  const count = feature.countByLocation[feature.locationKey]
   const zoom = nycOl.TILE_GRID.getZForResolution(resolution)
+  let count = 0
+  if (zoom > 13 && $('#tabs .btns .btn-2').hasClass('filtered')) {
+    const coord = feature.getGeometry().getCoordinates()
+    count = finderApp.source.getFeaturesAtCoordinate(coord).length
+    console.warn('count=',count);
+    
+  } else {
+    count = feature.countByLocation[feature.locationKey]
+  }
 
   let size = 12
   if (zoom > 11) size = 16
